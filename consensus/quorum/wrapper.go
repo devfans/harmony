e6c1464c1f9e8707f980045ee2db356cfbb01d54
Stefan Liu <stefanliu@outlook.com>
2022-02-24 10:52:55 +0800

    Move some funcs to quorum folder

--- consensus/quorum/wrapper.go
+++ consensus/quorum/wrapper.go
@@ -2,14 +2,21 @@ package quorum
 
 import (
 	"math/big"
+	"fmt"
 
 	"github.com/pkg/errors"
 
+	"github.com/harmony-one/harmony/internal/params"
 	"github.com/harmony-one/harmony/consensus/votepower"
-	"github.com/harmony-one/harmony/shard"
 	nodeconfig "github.com/harmony-one/harmony/internal/configs/node"
 	shardingconfig "github.com/harmony-one/harmony/internal/configs/sharding"
+	"github.com/harmony-one/harmony/shard"
 )
+
+type NetworkID = shardingconfig.NetworkID
+type ChainConfig = params.ChainConfig
+type Schedule = shardingconfig.Schedule
+type NetworkType = nodeconfig.NetworkType
 
 // NewVerifier creates the quorum verifier for the given committee, epoch and whether the scenario
 // is staking.
@@ -34,4 +41,24 @@ func newStakeVerifierWithConfig(networkType nodeconfig.NetworkType,
 	return &stakeVerifier{
 		r: *r,
 	}, nil
+}
+
+func GetNetworkConfigAndShardSchedule(id shardingconfig.NetworkID) (
+	schedule shardingconfig.Schedule,
+	networkType nodeconfig.NetworkType,
+	config *params.ChainConfig,
+	err error) {
+	switch id {
+	case shardingconfig.MainNet:
+		schedule = shardingconfig.MainnetSchedule
+		networkType = nodeconfig.Mainnet
+		config = params.MainnetChainConfig
+	case shardingconfig.TestNet:
+		schedule = shardingconfig.TestnetSchedule
+		networkType = nodeconfig.Testnet
+		config = params.TestChainConfig
+	default:
+		err = fmt.Errorf("invalid network id: %v", id)
+	}
+	return
 }
